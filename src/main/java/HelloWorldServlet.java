558a11b9081b8e1eb594bfca773dac2f4fb62e49
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-26 11:37:31 -0600

    completed exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,34 +9,54 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
-        HttpSession session = request.getSession(true);
-        response.setContentType("text/html");
-        PrintWriter out1 = response.getWriter();
-        int count = 0;
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String name = req.getParameter("name");
+
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println("<BIG>Hello World</BIG>");
+        out.println("<FORM METHOD=GET ACTION=\"/hello\">\n" +
+                "If you don't mind me asking, what is your name?\n" +
+                "<INPUT TYPE=TEXT NAME=\"name\"><P>\n" +
+                "<INPUT TYPE=SUBMIT>\n" +
+                "</FORM>");
+        out.println("Hello, " + name);
+        out.println("</BODY></HTML>");
+
+        HttpSession session = req.getSession(true);
+        res.setContentType("text/html");
+        PrintWriter out1 = res.getWriter();
+        Integer count = 0;
         String head;
         if (session.isNew()) {
             head = "This is the New Session";
         } else {
             head = "This is the old Session";
-            Integer oldCount = (Integer) session.getValue("count");
-            if (oldCount != null) {
-                count = oldCount + 1;
+            Integer oldcount = (Integer) session.getValue("count");
+            if (oldcount != null) {
+                count = oldcount + 1;
             }
         }
         session.putValue("count", count);
-        out1.println("<HTML><BODY BGCOLOR=\"#FDF5E6\">\n" +
+        out1.println("<HTML><BODY BGCOLOR=\"#00ff00\">\n" +
                 "<H2 ALIGN=\"CENTER\">" + head + "</H2>\n" +
                 "<TABLE BORDER=1 ALIGN=CENTER>\n"
                 + "<TR BGCOLOR=\"#FFAD00\">\n"
                 + "  <TH>Information Type<TH>Session Count\n"
-                + "<TR>\n" + " <TD>Total Session Access\n" +
+                + "<TR>\n" + " <TD>Total Session Accesses\n" +
                 "<TD>" + count + "\n" +
                 "</TABLE>\n"
                 + "</BODY></HTML>");
+    }
+
+    public String getServletInfo() {
+        return "A servlet that knows the name of the person to whom it's" +
+                "saying hello";
     }
 }
