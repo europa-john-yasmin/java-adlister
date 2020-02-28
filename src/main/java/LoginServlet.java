897db178281ec273ed5f8f23bf2dc8b2a9b6062e
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-28 09:43:20 -0600

    created servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username != null && username.equals("admin") && password != null && password.equals("password")) {
+            response.sendRedirect("/profile.jsp");
+        } else {
+            response.sendRedirect("/login.jsp");
+        }
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        if (request.getMethod().equalsIgnoreCase("post")) {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            if (username.equals("admin") && password.equals("password")) {
-                response.sendRedirect("/profile");
-            }
-        }
-    }
 }
