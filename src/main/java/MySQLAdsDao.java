169690ecf7f502b2b6c00a9bf61748ce2d9be8cf
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-03 17:26:33 -0600

    updated

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection = null;
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
